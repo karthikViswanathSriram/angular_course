12836a1911a26b3382ede897cc985a95b172115d
karthikviz2000 <karthikviz2000@gmail.com>
2024-06-05 11:09:59 -0700

    services starting code

--- course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './shopping-edit.component.css'
 })
 export class ShoppingEditComponent {
+  @Output() emitIngredient = new EventEmitter<Ingredient>();
+  @ViewChild('nameInput',{static:false}) nameInp;
+  @ViewChild('amountInput',{static:false}) amountInp;
 
+  addIngredient(){
+    this.emitIngredient.emit(new Ingredient(this.nameInp.nativeElement.value,this.amountInp.nativeElement.value));
+  }
 }
